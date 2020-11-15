d8d9a1617020f15452e35dbd5a0d7ee44d2a859f
meowiris <chuiris17@gmail.com>
2020-11-15 01:38:07 -0500

    added a ping function to check that it is online

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
+
 @client.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send( #sends user a dm with the message below
         f'Hi {member.name}, welcome to Space Ark!'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(TOKEN)
